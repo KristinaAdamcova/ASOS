fefbf0e5bbe0da51d69c55653529c729fd2a7402
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2024-11-17 21:15:43 +0100

    next-auth implementation (not tested)

---
+++ asos/auth.config.ts
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './lib/prisma';
+import { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/auth/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+} satisfies NextAuthConfig;
