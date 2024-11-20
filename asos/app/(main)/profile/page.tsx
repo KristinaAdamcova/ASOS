12614e4f2ecc9b956f7dca2ed38923771f17eddf
Ema Hrobarova <ema.hrobarova@twinzo.eu>
2024-11-20 17:58:13 +0100

    style: remove green background from the subpages

--- asos/app/(main)/profile/page.tsx
+++ asos/app/(main)/profile/page.tsx
@@ -51,7 +51,7 @@ export default function Profile() {
     };
 
     return (
-        <div className="bg-gradient-to-r from-green-300 via-green-400 to-green-500 flex justify-center items-center">
+        <div className="flex justify-center items-center">
             <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg w-full">
                 <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">Profile</h1>
                 <p className="text-gray-700 text-center mb-6">
--- asos/app/(main)/profile/ratings/page.tsx
+++ asos/app/(main)/profile/ratings/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Ratings() {
     return (
-        <div className="bg-gradient-to-r from-green-300 via-green-400 to-green-500 flex justify-center items-center">
+        <div className="flex justify-center items-center">
             <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg w-full">
                 <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">Ratings</h1>
                 <p className="text-gray-700 text-center mb-6">
