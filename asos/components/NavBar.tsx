8ac489e32acff5003871d6195ec17b13b7a86777
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2024-11-20 16:14:53 +0100

    navbar session fix v2

--- asos/components/NavBar.tsx
+++ asos/components/NavBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,6 +7,7 @@ import Link from "next/link";
 export default function NavBar() {
     const session = useSession();
 
+    console.log(session);
     return (
         <nav className="bg-white rounded-lg shadow dark:bg-gray-900 mb-5">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
@@ -39,7 +42,7 @@ export default function NavBar() {
                             <Link href="/sell"
                                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-lime-700 md:p-0 dark:text-white md:dark:hover:text-lime-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sell product</Link>
                         </li>
-                        {session ? (
+                        {session.status === 'authenticated' ? (
                             <>
                                 <li>
                                     <Link href="/profile"
